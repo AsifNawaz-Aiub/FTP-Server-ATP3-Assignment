6d17e3dde4883bb0cc779b8b261cbc0288eca169
Asif Nawaz <asifnawaz5926@gmail.com>
2020-11-27 04:08:39 +0600

    Added database & completed Movies Tab

--- controller/ftpsite.js
+++ controller/ftpsite.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const moviesModel = require.main.require("./models/moviesModel");
 
 router.get("/", (req,res ) => {
 
-    res.render("ftpsite/index");
+    moviesModel.getByAction(function (results) {
+       var actionMovies=results;
+       moviesModel.getByThriller(function (results) {
+        var thrillerMovies=results;
+        moviesModel.getByComedy(function (results) {
+            var comedyrMovies=results;
+            res.render("ftpsite/index",{actionMovies : actionMovies, thrillerMovies : thrillerMovies, comedyrMovies : comedyrMovies});
+            });
+ 
+         });
 
+      });
+   
 });
 
 
